92415dbbbacabc77d55f60b7e3e7ae7cb2d587db
volcano <volcano@example.com>
2024-03-27 21:13:34 +0530

    auth routes done

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=13)
+    password = models.BinaryField()
+    email = models.EmailField(null=True)
+    registered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,68 @@
 from django.shortcuts import render, HttpResponse
 import datetime
+from authentication.models import Account
+import json
+import bcrypt
+from django.http import JsonResponse
+import re
+import jwt
+from datetime import datetime, date, timedelta
 
 def register(request):
-    current_time = datetime.datetime.utcnow()
-    return HttpResponse(f"Test Response current time is {current_time}")
+    if request.method == "POST":
+        body = json.loads(request.body.decode('utf-8'))
+        if {"password", "first_name", "last_name", "phone_number"} > body.keys():
+            return HttpResponse(JsonResponse({"message" : "Incomplete request body"}), status=400)
+        if len(body["first_name"]) == 0:
+            return HttpResponse(JsonResponse({"message" : "First name can't be empty"}), status=406)
+        if len(body["password"]) <= 8:
+            return HttpResponse(JsonResponse({"message" : "Password length should be minimum 8"}), status=406)
+        try:
+            existingAccount = Account.objects.get(phone_number=body["phone_number"], registered=True)
+            return HttpResponse(JsonResponse({"message" : "Phone number is already registered please login"}), status=400)
+        except Account.DoesNotExist:
+            pass
+        hashpw = bcrypt.hashpw(body["password"].encode('utf-8'), bcrypt.gensalt())
+        newAccount = Account(first_name=body["first_name"], last_name=body["last_name"], phone_number=body["phone_number"], password=hashpw, registered=True)
+        if "email" in body:
+            if not re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', body["email"]):
+                return HttpResponse(JsonResponse({"message" : "Invalid email"}), status=406)
+            try:
+                existingAccount = Account.objects.get(email=body["email"])
+                return HttpResponse(JsonResponse({"message" : "Email is already registered please login"}), status=400)
+            except Account.DoesNotExist:
+                pass
+            newAccount.email = body["email"]
+        newAccount.save()
+        return HttpResponse(JsonResponse({"message" : "account created"}, status=201))
+    return HttpResponse(status=405)
 
 def login(request):
-    current_time = datetime.datetime.utcnow()
-    return HttpResponse(f"Test Response current time is {current_time}")
+    if request.method == "POST":
+        body = json.loads(request.body.decode('utf-8'))
+        if {"password", "phone_number"} <= body.keys():
+            try:
+                account = Account.objects.get(phone_number=body["phone_number"], registered=True)
+                if bcrypt.checkpw(body["password"].encode('utf-8'), account.password):
+                    return HttpResponse(JsonResponse({"token" : genToken(account.id)}, status=200))
+                return HttpResponse(JsonResponse({"message" : "Incorrect password"}, status=401))
+            except Account.DoesNotExist:
+                return HttpResponse(JsonResponse({"message" : "account with phone number doesn't exist"}, status=404))
+        if {"password", "email"} <= body.keys():
+            try:
+                account = Account.objects.get(email=body["email"])
+                return HttpResponse(JsonResponse({"message" : "Email is already registered please login"}), status=400)
+                if bcrypt.checkpw(body["password"].encode('utf-8'), account.password):
+                    return HttpResponse(JsonResponse({"token" : genToken(account.id)}, status=200))
+                return HttpResponse(JsonResponse({"message" : "Incorrect password"}, status=401))
+            except Account.DoesNotExist:
+                return HttpResponse(JsonResponse({"message" : "account with email doesn't exist"}, status=404))
+        HttpResponse(JsonResponse({"message" : "Incomplete request body"}), status=400)
+    return HttpResponse(status=405)
+
+def genToken(id):
+    return jwt.encode({"id" : id, "exp" : datetime.now() + timedelta(days=10) }, "secret")
+# print(jwt.decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZXhwIjoxNzEyNDEzMTUyfQ.x-yKLpdkv0s1uHg5uy7o3D5aq5HWDAp70Xn2W5wXCpc", "secret", algorithms="HS256"))
+
+print(Account.objects.all().values())
+print(len(Account.objects.all()))
